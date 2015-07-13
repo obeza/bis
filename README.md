3cdfd9209bcb359a783234a2f613f7a6ca9dce8f
obeza <o.bezaudin@gmail.com>
2015-07-13 08:14:56 -0400

    ajout d'un fichier readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# Bis
+> Manage your favorites files for download
+
+Bis is a simplistic NodeJS command that will download file from a favorite list that you can manage.
+
+Example :
+You just want **jquery.min.js** in your folder : **libs**, Bis will bring to you **only** the file you want.
+
+## Install
+
+```sh
+$ npm install -g bis
+```
+
+## Documentation
+**commands**
+- `bis list` list your favorite from website.
+- `bis add` add a url for a file in your favorite's list.
+- `bis rm <name>` remove a url in your favorite.
+- `bis dl <name> <directory>` download a file from your favorite. < directory > is optional.
+- `bis help` list commands for Bis.
+
+## License
+
+M.I.T
--- index.js
+++ index.js
@@ -1,67 +1,54 @@
 #! /usr/bin/env node
 
-//var jsonfile = require('jsonfile');
-var Download = require('download');
-//var data = require("./data.json")
+var fs = require('fs');
+var request = require('request');
+var progress = require('request-progress');
 var util = require('util')
 var prompt = require('prompt');
-
+var mkdirp = require('mkdirp');
 var Storage = require('cli-storage')
   , storage = new Storage('obez', {});
 
-
-//var maListe = jsonfile.readFileSync( file);
-
-
-// on supprime les 2 premiers arguments que que donne le tableau 'process.argv'
+// on supprime les 2 premiers arguments que donne le tableau 'process.argv'
 var userArgs = process.argv.slice(2);
 
 
-//storage.set( 'dataListe', data )
-
-/*	Traitement des arguments */
-
 var dataListe = storage.get( 'dataListe' );
-
-//console.log( 'foo = ' + data[0].nom );
-
-if (userArgs[0]==="help"){
-	//storage.set( 'foo', userArgs[1] );
-	console.log("\n Liste des arguments pour la commande. \n");
-	process.exit();
-}
 
 function onErr(err) {
     console.log(err);
     return 1;
-  }
+}
+
+function bisFindIndex(key){
+	return dataListe.map(function(e){ return e.nom;}).indexOf(key);
+}
+
+var bisCreationFichier = function(){
+	console.log("creation");
+};
+
+if (userArgs[0]==="help"){
+	console.log("\n Commands for Bis : \n");
+	console.log("add         add a url for a file in your favorite's list");
+	console.log("rm <name>   remove a url in your favorite");
+	console.log("list        list your favorite");
+	console.log("dl <name>   download a file from your favorite");
+	process.exit();
+}
 
 if (userArgs[0]==="add"){
 
 	prompt.start();
 
-  //
-  // Get two properties from the user: username and email
-  //
-  prompt.get(['nom', 'version', 'url'], function (err, result) {
-    //
-    // Log the results.
-    //
+	prompt.get(['nom', 'version', 'url'], function (err, result) {
+
     dataListe.push(result);
     storage.set( 'dataListe', dataListe );
 
     console.log('Vos favoris ont été mis à jour !');
     
-  });
-
-  
-/*
-	console.log("\n name ? : ", util.inspect(dataAjouter.nom));
-	console.log("\n version ? : ", util.inspect(dataAjouter.version));
-	console.log("\n url ? : ", util.inspect(dataAjouter.url));
-*/
-
-	//process.exit()
+  	});
 }
 
 if (userArgs[0]==="list"){
@@ -75,9 +62,7 @@ if (userArgs[0]==="list"){
 
 if (userArgs[0]==="rm"){
 	if (userArgs[1]){
-		
-		var pos = dataListe.map(function(e){ return e.nom;}).indexOf(userArgs[1]);
-
+		var pos = bisFindIndex(userArgs[1]);
 		dataListe.splice(pos, 1);
 		storage.set( 'dataListe', dataListe );
 		console.log('Vos favoris ont été mis à jour !');
@@ -90,22 +75,59 @@ if (userArgs[0]==="rm"){
 
 if (userArgs[0]==="dl"){
 	if (userArgs[1]){
-		//console.log('dl ' + userArgs[1]);
 		var dest = './';
 		if (userArgs[2]){
-			dest = userArgs[2];
+			dest =  userArgs[2] + "/";
 		}
-		dataListe.forEach(function(l){
-			if (l.nom===userArgs[1]){ 
-				console.log("check " + l.url);
-				new Download({mode: '755'})
-    				.get(l.url)
-    				.dest(dest)
-    				.run();
-			}
-			//console.log( " - " + l.nom + " v-" + l.version + "\r");
+		var pos = bisFindIndex(userArgs[1]);
+		var fileUrl = dataListe[pos].url;
+		var fileFs = dest + fileUrl.split("/").pop();
+		console.log('filefs ' + fileFs);
+		var mkdir = 'mkdir -p ' + dest;
+
+		// on check s'il y a un répertoire,
+		// si oui, on le créé
+		mkdirp(dest, function (err) {
+		    if (err) console.error(err)
 		});
-		//console.log('\n url : ' + maListe[])
+
+process.stdout.write('Downloading...');
+intervalId = setInterval(function() { process.stdout.write('.'); }, 1000);
+
+// Note that the options argument is optional 
+var request = request(fileUrl);
+progress(request, {
+    throttle: 2000,  // Throttle the progress event to 2000ms, defaults to 1000ms 
+    delay: 1000      // Only start to emit after 1000ms delay, defaults to 0ms 
+})
+.on('progress', function (state) {
+    console.log('received size in bytes', state.received);
+    // The properties bellow can be null if response does not contain 
+    // the content-length header 
+    console.log('total size in bytes', parseInt(request.response.headers['content-length']));
+    console.log('percent', state.percent);
+})
+.on('error', function (err) {
+    // Do something with err 
+})
+.pipe(fs.createWriteStream(fileFs))
+.on('error', function (err) {
+    // Do something with err 
+})
+.on('close', function (err) {
+    // Saved to doogle.png!
+    process.stdout.write(' done!\n');
+    clearInterval(intervalId);
+})
+
+/*
+fs.readdir( dest,function(err, file){
+	console.log("no exist");
+	//fs.createWriteStream(dest + "/" + fileFs));
+	})
+*/
+
+
 
 
 	} else {
